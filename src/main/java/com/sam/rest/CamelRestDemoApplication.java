0410e0754de83a2d76aff17a665082c37f0982af
samckp <sam.mca.liet@gmail.com>
2019-02-22 19:51:47 +0530

    Initial code base created

---
+++ src/main/java/com/sam/rest/CamelRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sam.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelRestDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CamelRestDemoApplication.class, args);
+	}
+}
