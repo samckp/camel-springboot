e7dfc7881176fcc23ea659fb729bb0e3cb92d9fb
samckp <sam.mca.liet@gmail.com>
2019-02-27 21:05:08 +0530

    Simple Route created !!

--- src/main/java/com/sam/rest/CamelRestDemoApplication.java
+++ src/main/java/com/sam/rest/CamelRestDemoApplication.java
@@ -1,9 +1,12 @@
 package com.sam.rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.sam.rest.route")
 public class CamelRestDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sam/rest/route/SimpleRoute.java
@@ -0,0 +1,17 @@
+package com.sam.rest.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("timer://mytimer?period=1s&repeatCount=5")
+                .routeId("timerRoute")
+                .setBody(simple("this is test msg !"))
+        .to("stream:out");
+    }
+}
