18bb6b9ac6f8b8bef71c444c44c03171860c3a57
samckp <sam.mca.liet@gmail.com>
2019-03-04 21:45:23 +0530

    ActiveMQ Route Added !!

--- src/main/java/com/sam/rest/CamelRestDemoApplication.java
+++ src/main/java/com/sam/rest/CamelRestDemoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan("com.sam.rest.route")
 @PropertySource("application.yml")
+@ImportResource("classpath:Beans.xml")
 public class CamelRestDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sam/rest/route/ActiveMQRoute.java
+++ src/main/java/com/sam/rest/route/ActiveMQRoute.java
@@ -12,5 +12,11 @@ public class ActiveMQRoute extends RouteBuilder {
         from("{{activeMQRoute}}")
                 .to("log:?level=INFO&showBody=true")
                 .to("log:sample");
+
+
+        from("activemq:queue:writeToMq")
+                .log("Messsage read from AMQ Queue : ${body}")
+                .transform(simple("Hello ${body}, Welcome to JavaOutOfBounds.com"))
+                .convertBodyTo(String.class, "UTF-8");
     }
 }
