f21cecf39e4650263fc32fa164730bb55a6d3971
samckp <sam.mca.liet@gmail.com>
2019-03-20 08:28:38 +0530

    code refactoring

--- src/main/java/com/sam/rest/route/RestletRoute.java
+++ src/main/java/com/sam/rest/route/RestletRoute.java
@@ -30,38 +30,43 @@ public class RestletRoute extends RouteBuilder {
                 .apiContextPath("/api-doc")
                 .apiProperty("api.title", "Example of REST API").apiProperty("api.version", "1.0")
                 .apiProperty("cors", "true")
-                .host("localhost").port("8888");
+                .host("localhost").port("8888")
+        ;
 
         rest("/employee")
                 .post().to("direct:postEmployees")
                 .get().to("direct:getEmployees").description("All Employees list")
                 .get("/{empId}").to("direct:getEmployeeId").description("Find Employee by Id")
                 .put("/{empId}").to("direct:putEmpId").description("Modify Emp by ID")
-                .delete("/{empId}").to("direct:deleteEmployeeId").description("Delete emp by Id");
+                .delete("/{empId}").to("direct:deleteEmployeeId").description("Delete emp by Id")
+        ;
 
         from("direct:getEmployees")
                 .setBody(simple("select * from employee"))
-                .log("${body}")
-                .to("jdbc:dataSource");
+                .to("jdbc:dataSource")
+        ;
 
         from("direct:getEmployeeId")
                 .setBody(simple("select * from employee where id = ${header.empId}"))
-                .log("post----->${body}")
-                .to("jdbc:dataSource");
+                .to("jdbc:dataSource")
+        ;
 
         from("direct:postEmployees")
                 .setBody(simple("INSERT INTO employee ( department, employee_name, employee_salary) VALUES "+
                         "('${body[department]}', '${body[employee_name]}', ${body[salary]})"))
-                .to("jdbc:dataSource");
+                .to("jdbc:dataSource")
+        ;
 
         from("direct:putEmpId")
                 .setBody(simple("update employee set department='${body[department]}', employee_name='${body[employee_name]}'," +
                         "employee_salary = ${body[salary]} where id = ${header.empId}"))
-                .to("jdbc:dataSource");
+                .to("jdbc:dataSource")
+        ;
 
         from("direct:deleteEmployeeId")
                 .setBody(simple("delete from employee where id = ${header.empId}"))
-                .to("jdbc:dataSource");
+                .to("jdbc:dataSource")
+        ;
     }
 }
 
